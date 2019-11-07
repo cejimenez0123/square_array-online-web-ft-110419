a30eb01309761851750ef3258a881be6b890fc13
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-07 17:23:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
 squared = []
 count = 0
-while count > array.size do
+while count < array.size do
   squared.push(array[count]**2)
   count +=1
   return squared
